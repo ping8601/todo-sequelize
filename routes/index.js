b70299b1d035c471cabeddcd1d326411b353d86e
ping8601 <wanping86701@gmail.com>
2022-11-19 11:43:29 +0800

    add detail page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const users = require('./modules/users')
 const home = require('./modules/home')
+const todos = require('./modules/todos')
 
 router.use('/users', users)
+router.use('/todos', todos)
 router.use('/', home)
 
 module.exports = router
