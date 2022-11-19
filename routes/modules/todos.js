0d27c3e4da5af500b338bd58f78460827ee8499b
ping8601 <wanping86701@gmail.com>
2022-11-19 23:16:02 +0800

    add edit function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -25,4 +25,31 @@ router.get('/:id', (req, res) => {
   .catch(error => console.log(error))
 })
 
+// edit todo
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const userId = req.user.id
+  return Todo.findOne({ 
+    where: {id, UserId: userId },
+    raw: true,
+    nest: true 
+  })
+  .then(todo => res.render('edit', { todo }))
+  .catch(error => console.error(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const userId = req.user.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, UserId: userId }})
+  .then( todo => {
+    todo.name = name
+    todo.isDone = isDone === 'on'
+    return todo.save()
+  })
+  .then(() => res.redirect(`/todos/${id}`))
+  .catch(error => console.error(error))
+})
+
 module.exports = router
