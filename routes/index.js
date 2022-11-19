30086ca2a39ee85f9eac4db6e2afbe11aa9451e9
ping8601 <wanping86701@gmail.com>
2022-11-19 23:54:33 +0800

    add facebook login feature

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const users = require('./modules/users')
 const home = require('./modules/home')
+const auth = require('./modules/auth')
 const todos = require('./modules/todos')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/todos', authenticator, todos)
 router.use('/', authenticator, home)
 
